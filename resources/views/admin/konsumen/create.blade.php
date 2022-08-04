a8989891fd53035ca7c64237360194ae81c7b966
Aditya Pratama Badra <orochimadit@gmail.com>
2022-08-04 11:18:11 +0700

    create konsumen

--- resources/views/admin/konsumen/create.blade.php
+++ resources/views/admin/konsumen/create.blade.php
@@ -23,58 +23,46 @@
                 <div class="row">
 
 
-                    <form action="{{route('users.store')}}" method="POST" class="form-horizontal" role="form" enctype="multipart/form-data">
-
-                        @csrf
+                    <form action="{{route('konsumen.store')}}" method="POST" class="form-horizontal" role="form" enctype="multipart/form-data">
                         <div class="col-md-6 form-horizontal">
+                            @csrf
                             <div class="form-group">
-                                <label class="col-md-2 control-label">Bank:</label>
-                                <div class="col-md-4">
-                                    
+                                <label class="col-md-2 control-label" for="exampleFormControlInput1">Nama</label>
+                                <div class="col-md-10">
+                                    <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Enter Name" name="name">
                                 </div>
+                                @error('name') <span class="text-danger error">{{ $message }}</span>@enderror
                             </div>
-                            <div class="col-md-12">
-                                <div class="form-group">
-                                    <label for="field-3" class="col-md-2 control-label">Jumlah Pinjaman</label>
-                                    <div class="col-md-10">
-                                        <input type="text" name="loan_amount" id="example-input3-group1" data-a-sign="Rp " name="pinjaman" class="form-control autonumber" placeholder="..">
-                                        <span class="font-13 text-muted">Rupiah</span>
-                                    </div>
+                            <div class="form-group">
+                                <label class="col-md-2 control-label">Email</label>
+                                <div class="col-md-10">
+                                    <input type="text" name="email" class="form-control">
                                 </div>
+                                @error('email') <span class="text-danger error">{{ $message }}</span>@enderror
                             </div>
-                            <div class="col-md-12">
-                                <div class="form-group">
-                                    <label for="field-1" class="col-md-2 control-label">Jangka Waktu</label>
-                                    <div class="col-md-10">
-                                        <select class="form-control" name="range_time">
-                                            <option value="10 Tahun">10 Tahun</option>
-                                            <option value="11 tahun">11 Tahun</option>
-                                            <option value="12 Tahun">12 Tahun</option>
-                                            <option value="13 Tahun">13 Tahun</option>
-                                            <option value="14 Tahun">14 Tahun</option>
-                                        </select>
-                                    </div>
+                            <div class="form-group">
+                                <label class="col-md-2 control-label" for="exampleFormControlInput1">Nomor Telepon</label>
+                                <div class="col-md-10">
+                                    <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Enter Phone number" name="phone_number">
                                 </div>
+                                @error('phone_number') <span class="text-danger error">{{ $message }}</span>@enderror
                             </div>
                         </div>
                         <div class="col-md-6 form-horizontal">
                             <div class="form-group">
-                                <label for="field-2" class="col-md-2 control-label">Bunga</label>
-                                <div class="col-md-4">
-                                    <select class="form-control" name="bank_interest">
-                                        <option value="6,75%">6,75%</option>
-                                        <option value="5%">5%</option>
-                                        <option>3%</option>
-                                        <option>4%</option>
-                                        <option>5%</option>
-                                    </select>
+                                <label class="col-md-2 control-label">Password</label>
+                                <div class="col-md-10">
+                                    <input type="password" placeholder="" name="password" class="form-control autonumber">
                                 </div>
+                                @error('password') <span class="text-danger error">{{ $message }}</span>@enderror
                             </div>
                             <div class="form-group">
-                                <label for="field-3" class="col-md-2 control-label">Angsuran</label>
-                                <input type="text" class="form-control autonumber" data-a-sign="Rp " name="installment" id="field-3">
+                                <label class="col-md-2 control-label">Alamat</label>
+                                <div class="col-md-9">
+                                    <input type="text"  name="address" class="form-control" >
+                                </div>
+                                @error('address') <span class="text-danger error">{{ $message }}</span>@enderror
                             </div>
-
                             <div class="clearfix pull-right m-t-15">
                                 <button type="submit" class="btn btn-pink btn-rounded waves-effect waves-light">Submit</button>
                                 <button class="btn btn-secondary bd-0">Cancel</button>
