55ce999351a943d7ed95d7674bba2d7e53c097df
Aditya Pratama Badra <orochimadit@gmail.com>
2022-08-30 13:28:57 +0700

    index report

--- resources/views/admin/report/index.blade.php
+++ resources/views/admin/report/index.blade.php
@@ -1,11 +1,4 @@
-<?php 
-error_reporting(0);
-session_start();
-if($_SESSION['status']!="login"){
-  $_SESSION['denied'] = 'Anda harus masuk dulu!';
-  header("location:index.php");
-}
-?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -134,90 +127,6 @@ if($_SESSION['status']!="login"){
 
 
 
-            <?php 
-            if(isset($_GET['page'])){
-              $page = $_GET['page'];
-
-              switch ($page) {
-                case 'refresh':
-                include "report-home.html";
-                break;
-                case 'home':
-                include "home.php";
-                break;
-                case 'perumahanrep':
-                include "report-perumahan.php";
-                break;
-                case 'kavelingrep':
-                include "report-kaveling.php";
-                break;
-                case 'transaksirep':
-                include "report-transaksi.php";
-                break;
-                case 'penjualanrep':
-                include "report-penjualan.php";
-                break;
-                case 'progresrep':
-                include "report-progres.php";
-                break;
-                case 'kaveling':
-                include "view-kaveling.php";
-                break;
-                case 'frmkaveling':
-                include "frm-kaveling.php";
-                break;
-                case 'perumahan':
-                include "view-perumahan.php";
-                break;
-                case 'frmperumahan':
-                include "frm-perumahan.php";
-                break;
-                case 'viewgaleri':
-                include "view-gallery.php";
-                break;
-                case 'frmgaleri':
-                include "frm-gallery.php";
-                break;
-                case 'viewkpr':
-                include "view-kpr.php";
-                break;
-                case 'frmkpr':
-                include "frm-kpr.php";
-                break;
-                case 'viewadmin':
-                include "view-admin.php";
-                break;
-                case 'frmadmin':
-                include "frm-admin.php";
-                break;
-                case 'viewmember':
-                include "view-member.php";
-                break;
-                case 'transaksi':
-                include "view-transaksi.php";
-                break;
-                case 'pembayaran':
-                include "view-pembayaran.php";
-                break;
-                case 'laporan':
-                include "view-laporan.php";
-                break;
-                case 'penjualan':
-                include "view-penjualan.php";
-                break;
-                case 'statistik':
-                include "view-statistik.php";
-                break;      
-                default:
-                echo "<center><h3>Maaf. Halaman tidak di temukan !</h3></center>";
-                break;
-              }
-            }else{
-              include "report-home.html";
-
-            }
-
-            ?>
 
           </div>
         </div>
